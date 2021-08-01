e20dcff6d33fad837606d8b927fb8eb3403b82d6
Lucas Garron <code@garron.net>
2021-07-31 21:26:12 -0700

    FREEZE.unindexed

--- src/both-call/wrapper.ts
+++ src/both-call/wrapper.ts
@@ -1,6 +1,6 @@
 import { parse } from "cubing/alg";
 
 console.log("wrapper started");
-new Worker("./worker.ts");
+new Worker(new URL("./worker.ts", import.meta.url), { type: "module" });
 
 console.log("wrapper call", parse("R U R'"));
